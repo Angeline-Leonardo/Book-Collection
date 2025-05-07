02760d3d3692162d1f7f492ab4ff7caf17cbe8b4
Angel <34836604+Angeline-Leonardo@users.noreply.github.com>
2025-05-08 02:09:15 +0800

    Delete dashboard/backend/tracker directory

--- dashboard/backend/tracker/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import LearningItem
-
-class LearningItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LearningItem
-        fields = '__all__'
